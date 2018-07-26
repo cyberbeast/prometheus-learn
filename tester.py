e7c6a447de41fe9fc7c2528c9a970811c5703749
Sandesh Gade <sandeshgade@gmail.com>
2018-07-26 14:32:09 -0400

    Updated tester code

--- tester.py
+++ tester.py
@@ -1,7 +1,29 @@
 import urllib.request
 import time
+import names
+import random
+from terminaltables import SingleTable
+import os
+import click
+
+click.clear()
+
+table_data = [['Request','Response','Post Sleep Time']]
+table = SingleTable(table_data)
+
+
 for i in range(2000):
-    with urllib.request.urlopen('http://localhost:5000/hello/sandesh') as response:
-        print(response.read())
-    time.sleep(0.1)
+    sleep_tmr = random.randint(0,1)
+    request = 'http://localhost:5000/hello/'
+    response_ = ''
+    click.clear()
+    table_data.append([request, response_, sleep_tmr])
+    print(table.table)
+    with urllib.request.urlopen(request + names.get_first_name()) as response:
+        response_ = str(response.read())
+    
+    click.clear()
+    table_data[len(table_data)-1] = [request, response_, sleep_tmr]
+    print(table.table)
+    time.sleep(sleep_tmr)
     
