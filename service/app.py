f742bc2c8ee7ecb578c3507127d15d24c43a7d19
Sandesh Gade <sandeshgade@gmail.com>
2018-07-26 17:34:58 -0400

    Added prometheus_flask_exporter

--- service/app.py
+++ service/app.py
@@ -1,59 +1,59 @@
 import random
 import time
 
-from flask import Flask, render_template_string, abort
-from prometheus_client import generate_latest, REGISTRY, Counter, Gauge, Histogram
+from flask import Flask, request, abort
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
-# A counter to count the total number of HTTP requests
-REQUESTS = Counter('http_requests_total', 'Total HTTP Requests (count)', ['method', 'endpoint', 'status_code'])
+# # A counter to count the total number of HTTP requests
+# REQUESTS = Counter('http_requests_total', 'Total HTTP Requests (count)', ['method', 'endpoint', 'status_code'])
 
-# A gauge (i.e. goes up and down) to monitor the total number of in progress requests
-IN_PROGRESS = Gauge('http_requests_inprogress', 'Number of in progress HTTP requests')
+# # A gauge (i.e. goes up and down) to monitor the total number of in progress requests
+# IN_PROGRESS = Gauge('http_requests_inprogress', 'Number of in progress HTTP requests')
 
-# A histogram to measure the latency of the HTTP requests
-TIMINGS = Histogram('http_request_duration_seconds', 'HTTP request latency (seconds)')
+# # A histogram to measure the latency of the HTTP requests
+# TIMINGS = Histogram('http_request_duration_seconds', 'HTTP request latency (seconds)')
+
+# Static information as metric.
+metrics.info('app_info', 'Application Info', version='1.0.3')
 
 
-# Standard Flask route stuff.
 @app.route('/')
-# Helper annotation to measure how long a method takes and save as a histogram metric.
-@TIMINGS.time()
-# Helper annotation to increment a gauge when entering the method and decrementing when leaving.
-@IN_PROGRESS.track_inprogress()
 def hello_world():
-    REQUESTS.labels(method='GET', endpoint="/", status_code=200).inc()  # Increment the counter
-    return 'Hello, World!'
+    return 'Hello, World!' # Requests tracked by default
 
-# Note I'm intentionally failing occasionally to simulate a flakey service.
-@app.route('/slow')
-@TIMINGS.time()
-@IN_PROGRESS.track_inprogress()
-def slow_request():
-    v = random.expovariate(1.0 / 1.3)
-    if v > 3:
-        REQUESTS.labels(method='GET', endpoint="/slow", status_code=500).inc()
-        abort(500)
-    time.sleep(v)
-    REQUESTS.labels(method='GET', endpoint="/slow", status_code=200).inc()
-    return render_template_string('<h1>Wow, that took {{v}} s!</h1>', v=v)
+@app.route('/skip')
+@metrics.do_not_track()
+def skip():
+    return 'ok'
+
+@app.route('/<item_type>')
+@metrics.do_not_track()
+@metrics.counter('invocation_by_type', 'Number of invocations by type', labels={'item_type': lambda: request.view_args['type']})
+def by_type(item_type):
+    return 'ok'
+
+@app.route('/long-running')
+@metrics.gauge('in_progress', 'Long running requests in progress')
+def long_running():
+    return 'ok'
+
+@app.route('/status/<int:status>')
+@metrics.do_not_track()
+@metrics.summary('requests_by_status', 'Request latencies by status', labels={'status': lambda r: r.status_code})
+@metrics.histogram('requests_by_status_and_path', 'Request latencies by status and path', labels={'status': lambda r: r.status_code, 'path': lambda: request.path})
+def echo_status(status):
+    return 'Status: %s' % status, status
 
 
-@app.route('/hello/<name>')
-@IN_PROGRESS.track_inprogress()
-@TIMINGS.time()
-def index(name):
-    REQUESTS.labels(method='GET', endpoint="/hello/<name>", status_code=200).inc()
-    return ('Hello %s!' % name)
-
-
-@app.route('/metrics/')
-@IN_PROGRESS.track_inprogress()
-@TIMINGS.time()
-def metrics():
-    REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
-    return generate_latest(REGISTRY)
+# @app.route('/metrics/')
+# @IN_PROGRESS.track_inprogress()
+# @TIMINGS.time()
+# def metrics():
+#     REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
+#     return generate_latest(REGISTRY)
 
 
 if __name__ == "__main__":
